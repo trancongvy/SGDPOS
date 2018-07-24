abea6192ff9edd7247b02aa0f692c3f0aa368040
trancongvy <trancongvy@gmail.com>
2018-07-24 11:11:45 +0700

    them phan bep

---
+++ CusPOS/fBep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+namespace CusPOS
+{
+    public partial class fBep : XtraForm
+    {
+        public fBep()
+        {
+            InitializeComponent();
+        }
+    }
+}
